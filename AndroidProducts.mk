b7f1224a53a65a1e43ce988e2d817d7adb65600b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-11-14 10:13:18 +0100

    TECNO-KE5k: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: tecno Model: TECNO KE5k Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_TECNO-KE5k.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_TECNO-KE5k-user \
+    omni_TECNO-KE5k-userdebug \
+    omni_TECNO-KE5k-eng
